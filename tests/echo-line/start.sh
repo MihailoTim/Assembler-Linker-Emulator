c2eb1f311132c18be91a00e0861aba120add62dd
MihailoTim <mihailo.tim@gmail.com>
2023-06-06 00:27:12 -0700

    Changed exceptions and fixed an error with -relocatable option for linker. Changed section placement so that all section appear after the last placed one

--- tests/echo-line/start.sh
+++ tests/echo-line/start.sh
@@ -8,8 +8,14 @@ ${ASSEMBLER} -o timer.o timer.s
 ${ASSEMBLER} -o terminal.o terminal.s
 ${ASSEMBLER} -o software.o software.s
 ${ASSEMBLER} -o printer.o printer.s
+${LINKER} -relocatable \
+  -o reloc1.o \
+  main.o printer.o software.o
+${LINKER} -relocatable \
+  -o reloc2.o \
+  handler.o terminal.o timer.o
 ${LINKER} -hex \
   -place=main@0x40000000 \
   -o program.hex \
-  handler.o main.o terminal.o timer.o software.o printer.o
+  reloc1.o reloc2.o
 ${EMULATOR} program.hex
